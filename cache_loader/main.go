b38774325efd7d6bb1b35322b0e30f20ba155076
NateMartes <nathaniel.martes.business@gmail.com>
2026-02-01 12:55:17 -0500

    feat: Update Lambda function definiton

--- cache_loader/main.go
+++ cache_loader/main.go
@@ -1,16 +1,50 @@
 package main
 
 import (
+	"fmt"
+	"context"
+	"runtime"
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 )
 
-func handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+func chunk_records(k int, records []events.KafkaRecord) [][]events.KafkaRecord {
+	
+	output := [][]events.KafkaRecord{}
+	
+	chunk := []events.KafkaRecord{}
+	chunkSize := -1
+	for _, record := range records {
+		if chunkSize + 1 == k {
+			chunkSize = -1
+			output = append(output, chunk)
+			chunk = []events.KafkaRecord{}
+		} else {
+			chunkSize++
+			chunk = append(chunk, record)
+		}
+	}
+	
+	if chunkSize + 1 == k && chunkSize != -1 {
+		output = append(output, chunk)
+	}
+	
+	return output
+}
 
-	return events.APIGatewayProxyResponse{
-		Body:       "test",
-		StatusCode: 200,
-	}, nil
+func process_records(records []events.KafkaRecord) {
+	cores := runtime.NumCPU()
+	recordsGrouped := chunk_records(cores, records)
+	fmt.Println(recordsGrouped)
+}
+
+func handler(ctx context.Context, event events.KafkaEvent) error {
+	
+	for _, records := range event.Records {
+		process_records(records)
+	}
+	
+	return nil
 }
 
 func main() {
