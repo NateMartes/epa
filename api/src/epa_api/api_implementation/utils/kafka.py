a716a017d6c607259a88213cdc9ae23293dea2f7
Nathaniel Martes <nathaniel.martes.business@gmail.com>
2026-02-12 16:34:48 -0500

    feat: Updated Kafka to use one topic and 3 consumer groups, added Kafka API to EPA API along with some functions

---
+++ api/src/epa_api/api_implementation/utils/kafka.py
@@ -0,0 +1,49 @@
+from kafka import KafkaProducer
+from typing import Dict, Any
+import os
+
+class KafkaUtils:
+    """A class with helpful methods to interact with Apache Kafka"""
+    
+    @staticmethod
+    def get_kafka_bootstrap_server() -> str:
+        """
+        Gets the kafka server this API will send messages to.
+        
+        :return: The bootstrap server in the format 'hostname:port'
+        :rtype: str
+        """
+        
+        hostname = os.getenv("EPA_KAFKA_BROKER_HOSTNAME")
+        port = os.getenv("EPA_KAFKA_BROKER_PORT")
+        
+        if not hostname or not port:
+            raise ValueError("Environment variables for Kafka not set")
+            
+        return f"{hostname}:{port}"
+        
+    @staticmethod
+    def connect_to_kafka_as_producer() -> KafkaProducer:
+        """
+        Connects to the Kafka as Kafka producer using the underlying envinroment variables.
+        
+        :return: A new kafka producer object
+        :rtype: kafka.KafkaProducer
+        """
+        server = KafkaUtils.get_kafka_bootstrap_server()
+        return KafkaProducer(
+            bootstrap_servers=server,
+            allow_auto_create_topics=False,
+            retries=3)
+        
+    @staticmethod
+    def send_message(producer: KafkaProducer, message: Dict[Any, Any]):
+        """
+        Sends a message to the Kafka queue.
+        
+        :param producer: A producer connected to a Kafka instance
+        :type prodcuer: kafka.KafkaProducer
+        :param message: The message to send
+        :type message: Dict[Any, Any]
+        """
+        ...
