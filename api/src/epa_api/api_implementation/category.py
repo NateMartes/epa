87c9e11608e81f8bc605c52c70b23c1b76f7c536
NateMartes <nathaniel.martes.business@gmail.com>
2026-02-14 13:10:07 -0500

    feat: Add GET/ DELETE/ and POST/ /v1/category/subscribe

--- api/src/epa_api/api_implementation/category.py
+++ api/src/epa_api/api_implementation/category.py
@@ -1,11 +1,12 @@
+from fastapi.exceptions import HTTPException
 from epa_api.apis.category_api import BaseCategoryApi
 from epa_api.models.category_list import CategoryList
+from epa_api.models.subscribe_category_request import SubscribeCategoryRequest
 from typing import Optional
 from pydantic import StrictStr
 from epa_api.api_implementation.utils.token import TokenUtils
 from epa_api.api_implementation.utils.category import CategoryUtils
 from epa_api.api_implementation.utils.mongo import MongoUtils
-import logging
 
 class PostAPIImplementation(BaseCategoryApi):
     async def get_categories(
@@ -23,21 +24,94 @@ class PostAPIImplementation(BaseCategoryApi):
         categories_collection = MongoUtils.get_categories_collection(db)
         query = CategoryUtils.build_category_query(category_id=category_id)
         results = CategoryUtils.get_categories(categories_collection, query=query, page_num=page_num_int, page_size=page_size)
-        results = list(results)
+        output = CategoryUtils.get_category_list(list(results), page_num=page_num_int, page_size=page_size)
         
-        output = CategoryList(page_number=page_num_int, page_size=page_size, categories=[])
-        for category in results:
-            print(category)
-            if isinstance(output.categories, list):
-                try:
-                    output.categories.append(
-                        {
-                            "category_id": category["category_id"],
-                            "category_name": category["category_name"]
-                        }
-                    )
-                except KeyError:
-                    logging.getLogger(__name__).warning(f"Failed to get document {category}. Unexpected structure")
+        client.close()
+        return output
+        
+    async def subscribe_to_category(
+        self,
+        subscribe_category_request: SubscribeCategoryRequest,
+    ) -> None:
+        """Subscribe a user to a category"""
+        
+        token = TokenUtils.validate_access_token_with_db()
+        user_id = TokenUtils.get_user_id(token)
+        
+        client, db = MongoUtils.get_mongodb_database_connection()
+        category_collection = MongoUtils.get_categories_collection(db)
+        user_collection = MongoUtils.get_user_collection(db)
+        
+        try:
+            CategoryUtils.validate_subscribe_category_request(
+                subscribe_category_request,
+                user_id,
+                category_collection,
+                user_collection
+            )
+        except HTTPException as e:
+            client.close()
+            raise e
+            
+        CategoryUtils.subscribe_user_to_category(user_id, user_collection, subscribe_category_request.category_id)
+        client.close()
+        
+    async def unsubcribed_user_from_category(
+        self,
+        subscribe_category_request: SubscribeCategoryRequest,
+    ) -> None:
+        """Unsubscribe a user from a category"""
+        
+        token = TokenUtils.validate_access_token_with_db()
+        user_id = TokenUtils.get_user_id(token)
+        
+        client, db = MongoUtils.get_mongodb_database_connection()
+        category_collection = MongoUtils.get_categories_collection(db)
+        user_collection = MongoUtils.get_user_collection(db)
+        
+        try:
+            CategoryUtils.validate_unsubscribe_category_request(
+                subscribe_category_request,
+                user_id,
+                category_collection,
+                user_collection
+            )
+        except HTTPException as e:
+            client.close()
+            raise e
+            
+        CategoryUtils.unsubscribe_user_from_category(user_id, user_collection, subscribe_category_request.category_id)
+        client.close()
+        
+    async def get_user_subscribed_categories(
+        self,
+        page_num:Optional[StrictStr],
+    ) -> CategoryList:
+        """Get users subscribed categories"""
+        
+        token = TokenUtils.validate_access_token_with_db()
+        user_id = TokenUtils.get_user_id(token)
+        
+        client, db = MongoUtils.get_mongodb_database_connection()
+        category_collection = MongoUtils.get_categories_collection(db)
+        user_collection = MongoUtils.get_user_collection(db)
+        
+        page_num_int = CategoryUtils.get_page_num_from_string(page_num)
+        page_size = CategoryUtils.get_page_size()
+        results = CategoryUtils.get_user_subscribed_categories(
+            user_id, 
+            user_collection, 
+            category_collection,
+            page_num_int,
+            page_size
+        )
+        
+        if not results:
+            results = []
+        else:
+            results = list(results)
+            
+        output = CategoryUtils.get_category_list(results, page_num=page_num_int, page_size=page_size)
         
         client.close()
         return output
--- api/src/epa_api/apis/category_api.py
+++ api/src/epa_api/apis/category_api.py
@@ -24,9 +24,10 @@ from fastapi import (  # noqa: F401
 
 from epa_api.models.extra_models import TokenModel  # noqa: F401
 from pydantic import Field, StrictStr
-from typing import Optional
+from typing import Any, Optional
 from typing_extensions import Annotated
 from epa_api.models.category_list import CategoryList
+from epa_api.models.subscribe_category_request import SubscribeCategoryRequest
 from epa_api.security_api import get_token_BearerAuth
 
 router = APIRouter()
@@ -56,3 +57,66 @@ async def get_categories(
     if not BaseCategoryApi.subclasses:
         raise HTTPException(status_code=500, detail="Not implemented")
     return await BaseCategoryApi.subclasses[0]().get_categories(page_num, category_id)
+
+
+@router.get(
+    "/v1/category/subscribe",
+    responses={
+        200: {"model": CategoryList, "description": "Successful subscription of category. Returns the new list of the user&#39;s subscribed categories"},
+    },
+    tags=["Category"],
+    summary="Get users subscribed categories",
+    response_model_by_alias=True,
+)
+async def get_user_subscribed_categories(
+    page_num: Annotated[Optional[StrictStr], Field(description="Start at a specfic page number (if not given, this is always 0)")] = Query(None, description="Start at a specfic page number (if not given, this is always 0)", alias="page_num"),
+    token_BearerAuth: TokenModel = Security(
+        get_token_BearerAuth
+    ),
+) -> CategoryList:
+    """Get users subscribed categories"""
+    if not BaseCategoryApi.subclasses:
+        raise HTTPException(status_code=500, detail="Not implemented")
+    return await BaseCategoryApi.subclasses[0]().get_user_subscribed_categories(page_num)
+
+
+@router.post(
+    "/v1/category/subscribe",
+    responses={
+        200: {"description": "Successful subscription of category."},
+    },
+    tags=["Category"],
+    summary="Subscribe a user to a category",
+    response_model_by_alias=True,
+)
+async def subscribe_to_category(
+    subscribe_category_request: SubscribeCategoryRequest = Body(None, description=""),
+    token_BearerAuth: TokenModel = Security(
+        get_token_BearerAuth
+    ),
+) -> None:
+    """Subscribe a user to a category"""
+    if not BaseCategoryApi.subclasses:
+        raise HTTPException(status_code=500, detail="Not implemented")
+    return await BaseCategoryApi.subclasses[0]().subscribe_to_category(subscribe_category_request)
+
+
+@router.delete(
+    "/v1/category/subscribe",
+    responses={
+        200: {"description": "Successful unsubscribed a user from a category."},
+    },
+    tags=["Category"],
+    summary="Unsubscribe user from category",
+    response_model_by_alias=True,
+)
+async def unsubcribed_user_from_category(
+    subscribe_category_request: SubscribeCategoryRequest = Body(None, description=""),
+    token_BearerAuth: TokenModel = Security(
+        get_token_BearerAuth
+    ),
+) -> None:
+    """Unsubscribe a user from a category"""
+    if not BaseCategoryApi.subclasses:
+        raise HTTPException(status_code=500, detail="Not implemented")
+    return await BaseCategoryApi.subclasses[0]().unsubcribed_user_from_category(subscribe_category_request)
