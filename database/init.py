20d481b33a1a5ea067d6c8a0bdc1dc228ce722d9
NateMartes <nathaniel.martes.business@gmail.com>
2026-02-22 11:30:06 -0500

    fix: Update init script

--- database/init.py
+++ database/init.py
@@ -6,40 +6,8 @@ import pymongo
 import json
 import sys
 import os
+import time
 
-hostname = os.getenv("EPA_MONGODB_HOSTNAME")
-port = os.getenv("EPA_MONGODB_PORT")
-username = os.getenv("EPA_MONGODB_USERNAME")
-password = os.getenv("EPA_MONGODB_PASSWORD")
-dbname = os.getenv("EPA_MONGODB_DATABASE_NAME")
-if not hostname or \
-    not port or \
-    not username or \
-    not password or \
-    not dbname:
-    print("All Environment vairables are not set", file=sys.stderr)
-    sys.exit(1)
-    
-config_file = {}
-with open("config.json", "r") as file:
-    config_file = json.loads(file.read())
-    
-uri = f"mongodb://{username}:{password}@{hostname}:{port}/"
-client = None
-
-tries = 10
-while True:
-    try:
-        print(f"Waiting for response from {hostname}:{port} ...")
-        client = pymongo.MongoClient(uri, serverSelectionTimeoutMS=10000)
-        break
-    except errors.ConnectionFailure:
-        tries -= 1
-        if tries == 0:
-            print(f"Failed to connect to {hostname}:{port} after 10 tries", file=sys.stderr)
-            sys.exit(1)
-            
-print("Connected.")
 def get_index_defition(map):
     
     name = map.get("field", "")
@@ -71,41 +39,85 @@ def create_standard_index(collection, index):
     collection.create_index(index_defition, 
         unique=index.get("unique", False), 
         sparse=index.get("sparse", False))
-try:
-    client.admin.command('ping')
-    dbs = client.list_database_names()
-    for name in dbs:
-        if name == dbname:
-            print("Database already exists, aborting")
-            sys.exit(0)
     
-    db = client[dbname]
-    for collection in config_file.get("collections", []):
+def main():
+    
+    hostname = os.getenv("EPA_MONGODB_HOSTNAME")
+    port = os.getenv("EPA_MONGODB_PORT")
+    username = os.getenv("EPA_MONGODB_USERNAME")
+    password = os.getenv("EPA_MONGODB_PASSWORD")
+    dbname = os.getenv("EPA_MONGODB_DATABASE_NAME")
+    if not hostname or \
+        not port or \
+        not username or \
+        not password or \
+        not dbname:
+        print(f"All Environment vairables are not set. Variables:\n \
+            EPA_MONGODB_HOSTNAME={hostname}\n \
+            EPA_MONGODB_PORT={port}\n \
+            EPA_MONGODB_USERNAME={username}\n \
+            EPA_MONGODB_PASSWORD={password}\n \
+            EPA_MONGODB_DATABASE_NAME={dbname}\n \
+        ", file=sys.stderr)
+        sys.exit(1)
         
-        db.create_collection(collection.get("name", ""))
-        new_collection = db[collection.get("name", "")]
-        for idx in collection.get("indexes", []):
-            if idx.get("compound",None):
-                create_compound_index(new_collection, idx.get("fields", []))
-            else:
-                field_name = idx.get("field", "")
-                if idx.get("expireAfterSeconds", None):
-                    create_index_with_expire_time(new_collection, idx)
+    config_file = {}
+    with open("config.json", "r") as file:
+        config_file = json.loads(file.read())
+        
+    uri = f"mongodb://{username}:{password}@{hostname}:{port}/"
+    client = pymongo.MongoClient(uri, serverSelectionTimeoutMS=2000)
+    
+    tries = 10
+    while True:
+        try:
+            print(f"Waiting for response from {hostname}:{port} ...", flush=True)
+            client.admin.command("ping")
+            break
+        except errors.ConnectionFailure:
+            tries -= 1
+            if tries == 0:
+                print(f"Failed to connect to {hostname}:{port} after 10 tries", file=sys.stderr)
+                sys.exit(1)
+            time.sleep(10)
+                
+    print("Connected.")
+    
+    try:
+        dbs = client.list_database_names()
+        for name in dbs:
+            if name == dbname:
+                print("Database already exists, aborting")
+                sys.exit(0)
+        
+        db = client[dbname]
+        for collection in config_file.get("collections", []):
+            
+            db.create_collection(collection.get("name", ""))
+            new_collection = db[collection.get("name", "")]
+            for idx in collection.get("indexes", []):
+                if idx.get("compound",None):
+                    create_compound_index(new_collection, idx.get("fields", []))
                 else:
-                    create_standard_index(new_collection, idx)
-                    
-        documents = collection.get("init", [])
-        if documents:
-            new_collection.insert_many(documents)
-                    
-    print(f"MongoDB database at {hostname}:{port} initialized")
-    
-except Exception as e:
-    
-    print(f"error: Failed to initialize MongoDB at {hostname}:{port}, {e}", file=sys.stderr)
-    sys.exit(1)
-    
-finally:
-    client.close()
-    
+                    if idx.get("expireAfterSeconds", None):
+                        create_index_with_expire_time(new_collection, idx)
+                    else:
+                        create_standard_index(new_collection, idx)
+                        
+            documents = collection.get("init", [])
+            if documents:
+                new_collection.insert_many(documents)
+                        
+        print(f"MongoDB database at {hostname}:{port} initialized")
+        
+    except Exception as e:
+        
+        print(f"error: Failed to initialize MongoDB at {hostname}:{port}, {e}", file=sys.stderr)
+        sys.exit(1)
+        
+    finally:
+        client.close()
+        
+if __name__ == "__main__":
+    main()
     
