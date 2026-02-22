8dd96344ea255a4234a266c7a53549d2775cc76e
NateMartes <nathaniel.martes.business@gmail.com>
2026-02-22 12:26:48 -0500

    fix: Update terraform variable name

--- database/terraform/outputs.tf
+++ database/terraform/outputs.tf
@@ -2,8 +2,8 @@ output "mongo_db_cluster_dns_name" {
   value = module.networking.mongo_lb.dns_name
 }
 
-output "mongo_db_cluster_dns_namespace" {
-  value = module.networking.private_dns_namespace
+output "mongo_db_cluster_dns_namespace_name" {
+  value = module.networking.private_dns_namespace.name
 }
 
 output "mongo_db_cluster_dns_discovery_service" {
