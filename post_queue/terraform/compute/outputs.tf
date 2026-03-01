cb0a3e4598abe30e7b64dbf417845357033f9eab
NateMartes <nathaniel.martes.business@gmail.com>
2026-03-01 09:14:08 -0500

    feat: Add workflows for kafka, Add terraform outputs

--- post_queue/terraform/compute/outputs.tf
+++ post_queue/terraform/compute/outputs.tf
@@ -6,6 +6,6 @@ output "kafka_cluster" {
   value = aws_ecs_cluster.epa_kafka_cluster
 }
 
-output "first_mongodb_service" {
+output "first_kafka_service" {
   value = aws_ecs_service.epa_kafka_service[0].name
 }
--- post_queue/terraform/outputs.tf
+++ post_queue/terraform/outputs.tf
@@ -1,15 +1,7 @@
-output "kafka_cluster_dns_namespace_name" {
-  value = module.networking.private_dns_namespace.name
-}
-
-output "kafka_cluster_dns_discovery_service" {
-  value = module.networking.kafka_discovery_service
-}
-
 output "kafka_cluster_name" {
   value = module.compute.kafka_cluster.name
 }
 
-output "first_mongodb_service" {
-  value = module.compute.first_mongodb_service
+output "first_kafka_service" {
+  value = module.compute.first_kafka_service
 }
