269f77f1e6473af155ded15ec9033036056d9cd8
Aiden West <aiden.d.west@gmail.com>
2026-04-28 12:15:52 -0400

    final.final.final

--- client/components/SignUp.tsx
+++ client/components/SignUp.tsx
@@ -41,8 +41,7 @@ export default function MyForm() {
     	 
 	    setPassword('');
             setPasswordTwo('');
-       	    setUsername('');
-	    setEmail('');
+       	    
     	 }
 	 else {
 	 router.navigate('/login');	 
